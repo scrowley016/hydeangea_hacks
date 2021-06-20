a1c6f261e70438fddbbea548296532f01e310ae0
scrowley016 <33128271+scrowley016@users.noreply.github.com>
2021-06-20 06:36:10 -0400

    node

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import Image from 'react-bootstrap/Image'
 
-const Home =()=>{
-    
+
+const Home=()=>{
         return (
-            <Map
-              google={this.props.google}
-              zoom={8}
-              style={mapStyles}
-              initialCenter={{ lat: 47.444, lng: -122.176}}
-            />
+           <div>
+           <h1>Welcome To Walkology</h1>
+           </div>
+           
         );
 }
 
